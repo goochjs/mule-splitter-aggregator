4dd24d831ef287a57f81d9e3bf1202ea88aa7b18
Jeremy Gooch <goochjs@yahoo.co.uk>
2015-04-08 14:03:30 +0100

    Now routes to separate JMS flows based on message content

---
+++ src/main/java/org/goochjs/muleSplitterAggregator/MuleSplitterAggregatorException.java
@@ -0,0 +1,15 @@
+package org.goochjs.muleSplitterAggregator;
+
+public class MuleSplitterAggregatorException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1278828568929538638L;
+
+	public MuleSplitterAggregatorException() {}
+	
+	public MuleSplitterAggregatorException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/org/goochjs/muleSplitterAggregator/ThrowException.java
@@ -0,0 +1,10 @@
+package org.goochjs.muleSplitterAggregator;
+
+/**
+ * Throws an exception
+ */
+public class ThrowException {
+	public void throwException(Object thing) {
+		throw new MuleSplitterAggregatorException();
+	}
+}
